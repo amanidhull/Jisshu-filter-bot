023996bc7fd3f3b2b565d7fb5f601c0f4deb60eb
Amandeep <amanidhull63@gmail.com>
2025-02-12 23:16:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n⚡️Join ➥「<a href="https://t.me/Mslanders">MSLANDERS</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n<blockquote>⚡️Join ➥「 @MSLANDERS 」</blockquote></b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
